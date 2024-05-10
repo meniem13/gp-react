2d3da070b6cea263427282b84c03097cc9998d5b
meniem13 <meniemnasr84@gmail.com>
2024-05-10 16:50:55 +0300

    dsa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet'; // Import Helmet
 import './App.css';
 import Hero from './hero'; // Import the Hero component
 import Body from './body'; // Import the Body component
@@ -6,6 +7,17 @@ import Body from './body'; // Import the Body component
 const App = () => {
   return (
     <div className="App bg-background">
+      <Helmet>
+        <title>Your App Title</title>
+        <meta name="description" content="Description of your app.esp assistant to help in automate the hardware process with mobile app " />
+        <meta name="keywords" content="esp, assistant, app" />
+        <meta name="author" content="Your Name" />
+        <meta property="og:title" content="Your App Title" />
+        <meta property="og:description" content="Description of your app." />
+        <meta property="og:image" content="URL_TO_YOUR_APP_IMAGE" />
+        <meta property="og:url" content="https://esp-assistant.netlify.app/" />
+        <meta property="og:type" content="website" />
+      </Helmet>
       <Hero />
       <Body />
     </div>
@@ -13,6 +25,7 @@ const App = () => {
 };
 
 export default App;
+
 // document.getElementById('downloadButton').addEventListener('click', function() {
 //   var link = document.createElement('a');
 //   link.href = 'path/to/your/installer.exe'; // Update this path
